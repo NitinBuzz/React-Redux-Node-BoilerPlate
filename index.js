f93632bb3de63a2ebefca6a1fcc7d6604d69ee6d
NitinBuzz <reachnitinvenkat@gmail.com>
2018-08-12 13:08:37 +0530

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+require('./firebase');
+var nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/server', function(req, res) {
+  res.send('Hello World! from server');
+});
+
+app.post('/api/user', function(req, res) {
+  console.log(req);
+});
+
+app.get('/api/proxy/my/path', function(req, res) {
+  res.send('Hello Worldzzzz! from server');
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/public'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
